ef17de75a5af8257f62c5e04ed91b7767e48fd80
Sahil Naik <nsahil992@gmail.com>
2025-02-03 18:02:13 +0530

    feat: Terraform basics

---
+++ 01TerraformBasics/03InputVariables/variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+	default = "/Users/Sahil/Desktop/Terraform-for-Rookies/01TerraformBasics/03InputVariables/pets.txt"
+}
+
+variable "content" {
+  default = "We love pets!"
+}
+
+variable "prefix" {
+  default = "Mr"
+}
+
+variable "separator" {
+  default = "."
+}
+
+variable "length" {
+  default = "1"
+}
