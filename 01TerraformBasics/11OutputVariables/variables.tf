428e758cfb7658edc8f3988d90d2d8b86e4668b6
Sahil Naik <nsahil992@gmail.com>
2025-02-11 18:06:16 +0530

    feat: Terraform basics

---
+++ 01TerraformBasics/11OutputVariables/variables.tf
@@ -0,0 +1,20 @@
+variable "filename" {
+	default = "/Users/Sahil/Desktop/Terraform-for-Rookies/01TerraformBasics/11OutputVariable/pet.txt"
+}
+
+variable "content" {	
+	default = "I love pets"
+}
+
+variable "prefix" {	
+	default = "Mr"
+}
+
+variable "separator" {	
+	default = "."
+}
+
+variable "length" {	
+	default = "1"
+}
+
