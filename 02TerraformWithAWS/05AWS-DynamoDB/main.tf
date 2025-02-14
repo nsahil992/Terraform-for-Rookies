7de2fc884adf2dd132c0e7d7970d5ecd43c729d5
Sahil Naik <nsahil992@gmail.com>
2025-02-14 17:32:39 +0530

    feat: Terraform with AWS

---
+++ 02TerraformWithAWS/05AWS-DynamoDB/main.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "cars" {
+	name = "cars"
+	hash_key = "VIN"
+	billing_mode = "PAY_PER_REQUEST"
+	attribute = {
+	  name = "VIN"
+	  type = "S"
+	}
+}
+
+resource "aws_dynamodb_table_item" "car-items" {
+	table_name = aws_dynamodb_table.cars.name
+	hash_key = aws_dynamodb_table.cars.hash_key
+	item = <<EOF
+
+	{ 
+		"Manufacturer": {"S": "Toyota"},
+		"Model": {"S": "Lexus"},
+		"Year": {"N": 2024},
+		"VIN": {"S": "S9923482723H492038"},
+	}
+	EOF
+}
